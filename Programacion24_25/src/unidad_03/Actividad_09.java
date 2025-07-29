629b4855bd36dc2e73ec4e0365b51a067fb7f198
CodewithFreper <codewithfreper@gmail.com>
2025-07-29 12:54:13 +0200

    Unidad 03 -Actividad 09

--- Programacion24_25/src/unidad_03/Actividad_09.java
+++ Programacion24_25/src/unidad_03/Actividad_09.java
@@ -17,6 +17,10 @@ public class Actividad_09 {
 		int suma_pares = 0;
 		int numero_de_impares = 0;
 		int suma_impares = 0;
+		int multiplos4 = 0;
+		int suma_multiplos4 = 0;
+		int multiplos7 = 0;
+		int suma_multiplos7 = 0;
 		
 		
 		
@@ -37,12 +41,32 @@ public class Actividad_09 {
 				// 		System.out.println("La suma de los números impares es: " + suma_impares);	
 				
 			}
+			if (i % 4 == 0) {
+				multiplos4++;
+				//		System.out.println("Hay " + multiplos4 + " números múltiplos de 4.");   SI SE PONEN AQUÍ LOS SYSTEM.OUT.PRINTLN SE IMPRIMEN CADA VEZ QUE PASAN POR EL BUCLE
+				suma_multiplos4 +=i;
+				// 		System.out.println("La suma de los múltiplos de 4 es: " + suma_multiplos4);	
+				
+			}
+			else if (i % 7 == 0) {
+				multiplos7++;
+				//		System.out.println("Hay " + multiplos7 + " números múltiplos de 7.");   SI SE PONEN AQUÍ LOS SYSTEM.OUT.PRINTLN SE IMPRIMEN CADA VEZ QUE PASAN POR EL BUCLE
+				suma_multiplos7 +=i;
+				// 		System.out.println("La suma de los múltiplos de 7 es: " + suma_multiplos7);	
+				
+			}
 		}
 		System.out.println("Hay " + numero_de_pares + " números pares.");
 		System.out.println("La suma de los números pares es: " + suma_pares);
 		
 		System.out.println("Hay " + numero_de_impares + " números impares.");
 		System.out.println("La suma de los números impares es: " + suma_impares);
+		
+		System.out.println("La suma de los múltiplos de 4 es: " + suma_multiplos4);	
+		System.out.println("Hay " + multiplos4 + " números múltiplos de 4.");
+		
+		System.out.println("La suma de los múltiplos de 7 es: " + suma_multiplos7);	
+		System.out.println("Hay " + multiplos7 + " números múltiplos de 7.");
 	}
 
 }
