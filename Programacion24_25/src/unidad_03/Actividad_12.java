2572d89ba81fee7b01b99908e8f727c6ac889463
CodewithFreper <codewithfreper@gmail.com>
2025-08-09 19:49:58 +0200

    Unidad 03 - Actividad 12

--- Programacion24_25/src/unidad_03/Actividad_12.java
+++ Programacion24_25/src/unidad_03/Actividad_12.java
@@ -1,5 +1,7 @@
 package unidad_03;
 
+import java.util.Scanner;
+
 public class Actividad_12 {
 
 	public static void main(String[] args) {
@@ -10,6 +12,31 @@ public class Actividad_12 {
 		 * o no.
 		 */
 
+		int num;
+		int i;
+		boolean esPrimo = true;
+		Scanner lector = new Scanner(System.in);
+		
+		do {
+			System.out.println("Por favor, introduce un número entero:");
+			num = lector.nextInt();
+		} while (num < 0);
+		
+		if (num == 0 || num == 1) {
+			System.out.println(num + " no es un número primo");
+		} else if (num > 0) {
+			for (i = 2; i < num; i++) {
+				if (num%i == 0) {
+					esPrimo = false;
+					break;
+				}
+			}
+		}
+		if (esPrimo) {
+			System.out.println("Es primo");
+		} else {
+			System.out.println("No es primo");
+		}
 	}
 
 }
