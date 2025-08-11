814f00e6a65a96aa8a46f78d58f3585b78317796
CodewithFreper <codewithfreper@gmail.com>
2025-08-11 22:02:32 +0200

    Unidad 03 - Actividad 14

--- Programacion24_25/src/unidad_03/Actividad_14.java
+++ Programacion24_25/src/unidad_03/Actividad_14.java
@@ -15,7 +15,42 @@ public class Actividad_14 {
 		 * Número de caras: 500627 (50.0627%)
 		 * Número de cruces: 499373 (49.9373%)
 		 */
+ 
+		int caras = 0;
+		int cruces = 0;
+		int veces = 1000000;
+		double porcentCaras;
+		double porcentCruces;
+		int i;
+		double aleatorio;
+		
+		System.out.println("Lanzamiento de moneda");
+		System.out.println(".\n.\n.\n.\n.\n.\n.\n.\n");
+		
+		for (i = 0 /*1*/; i </*=*/veces; i++) { // Al inicializar la i en 0, cuando había puesto i <= veces, se ejecutaba 1000001 veces, por lo que el chivato de la comprobación nos decía que no era correcto.
+			aleatorio = Math.random();
+			if (aleatorio < 0.5) {
+				caras++;
+			} else {
+				cruces++;
+			}
+		}
+		
+		porcentCaras = caras * 100 / veces; //si ponemos caras / veces * 100 dará 0 porque redondea antes de multiplicar
+		porcentCruces = cruces * 100 / veces ;
+		
+		
+		System.out.println("Número de caras: " + caras + "(" + porcentCaras + ")\nNúmero de cruces: " + cruces + "(" + porcentCruces + ")");
+		
+		if(caras + cruces == veces) {
+			System.out.println("Los resultados han sido comprobados y son correctos.");
+		} else {
+			System.out.println("Los resultados no son correctos.");
+		}
 
+		
+		
 	}
 
 }
+// FUNCIONA
