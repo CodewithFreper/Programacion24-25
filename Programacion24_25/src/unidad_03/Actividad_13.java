727134e6edf0e621f6b0bfd4ebc856d6a3d479f8
CodewithFreper <codewithfreper@gmail.com>
2025-08-11 11:58:10 +0200

    Unidad 3 - Actividad 13

--- Programacion24_25/src/unidad_03/Actividad_13.java
+++ Programacion24_25/src/unidad_03/Actividad_13.java
@@ -1,5 +1,7 @@
 package unidad_03;
 
+import java.util.Scanner;
+
 public class Actividad_13 {
 
 	public static void main(String[] args) {
@@ -11,7 +13,47 @@ public class Actividad_13 {
 		 * 
 		 * Ejemplo: si el número es 36, deberá de escribir 36=2 * 2 * 3 * 3
 		 */
+		
+		int num;
+		int i;
+		boolean esPrimo = true;
+		int numUsuario;
+		Scanner lector = new Scanner(System.in);
+		
+		do {
+			System.out.println("Por favor, introduce un número enero:");
+			num = lector.nextInt();
+		} while (num < 0);
+		
+		
+		if (num == 0 || num == 1) {
+			System.out.println(num + " no tiene descomposición en factores primos.");
+		} else if (num > 0) {
+			for (i = 2; i < num; i++) {
+				if (num%i == 0) {
+					esPrimo = false;
+					break;
+				}
+			}
+		}
+		if (esPrimo) {
+			System.out.println("Es primo");
+		} else {
+			numUsuario = num; 
+			System.out.print(num + " = ");
+			for (i = 2; i<=num; i++) {
+				while (num % i == 0) {
+					System.out.print(i);
+					num = num/i;
+					if (num > 1) {
+						System.out.print(" * ");
+					}
+				}
+			}
+		}
 
 	}
 
 }
+
+//FUNCIONA
