987afd90285a47123d92878a6aaa4e59fb19c7e0
CodewithFreper <codewithfreper@gmail.com>
2025-08-14 11:07:55 +0200

    Unidad 04_Actividad 01

---
+++ Programacion24_25/src/unidad_04/Actividad_01.java
@@ -0,0 +1,16 @@
+package unidad_04;
+
+public class Actividad_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/*Escribe un método que devuelva el mayor de tres números que se piden al
+		  usuario.
+		*/
+		
+		
+
+	}
+
+}
